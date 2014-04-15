0dff5129d4f7a9e2fac3b53820c20b60dc134b05
Arash <Arash@172.30.33.83>
2014-04-15 14:03:49 -0400

    current params added to state

--- src/main/java/cloudservices/brokerage/policy/policycommons/model/entities/State.java
+++ src/main/java/cloudservices/brokerage/policy/policycommons/model/entities/State.java
@@ -4,6 +4,7 @@
  */
 package cloudservices.brokerage.policy.policycommons.model.entities;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,11 +16,13 @@ public class State extends UniObject {
 
     private int number;
     private Set<Proposition> propositions;
+    private HashMap<String,Object> params;
 
     public State() {
         super.setUniId((long) number);
         super.setUniName("state");
         this.propositions = new HashSet<>();
+        this.params=new HashMap<>();
     }
 
     public boolean addProposition(Proposition prop) {
@@ -28,6 +31,14 @@ public class State extends UniObject {
 
     public boolean removeProposition(Proposition prop) {
         return this.propositions.remove(prop);
+    }
+    
+    public Object addParam(String key,Object value){
+        return this.params.put(key, value);
+    }
+    
+    public Object removeParam(String key){
+        return this.params.remove(key);
     }
 
     public int getNumber() {
@@ -38,4 +49,21 @@ public class State extends UniObject {
         this.number = number;
         super.setUniId((long) number);
     }
+
+    public Set<Proposition> getPropositions() {
+        return propositions;
+    }
+
+    public void setPropositions(Set<Proposition> propositions) {
+        this.propositions = propositions;
+    }
+
+    public HashMap<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, Object> params) {
+        this.params = params;
+    }
+    
 }
